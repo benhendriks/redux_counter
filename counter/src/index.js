38b8834d123e65efc796b1b2a8d4ab245246df6b
Benjamin Steidl <localartpool@gmail.com>
2020-07-29 12:07:24 +0200

    Created new folder an started with the counter

--- counter/src/index.js
+++ counter/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import allReducer from './reducers';
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
